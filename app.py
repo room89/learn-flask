847cffa357c059b886a547625b5866ce884d2937
finmi <finmipt@gmail.com>
2020-05-13 01:22:25 +0300

    feat common: add sum handler

--- app.py
+++ app.py
@@ -1,21 +1,27 @@
 import math
 
 from flask import Flask, request, jsonify
+from time import sleep
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
+    sleep(60)
     return 'Hello World!'
 
 
-@app.route('/sqrt', methods=['GET'])
+@app.route('/summa', methods=['GET'])
 def add_message():
     print(request.args)
     if 'value' not in request.args:
         return 'No value in request', 400
-    response = {'result': math.sqrt(float(request.args['value']))}
+    newer = request.args['value'].split('_')
+    summa = 0
+    for i in newer:
+        summa += int(i)
+    response = {'result': summa, 'Message': 'Evthg is fuckin awesome'}
     return jsonify(response)
 
 
