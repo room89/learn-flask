29c91413b9942cc9ede920e5cdce8f397b39edbd
finmipt <finmipt@gmail.com>
2020-05-17 19:23:37 +0300

    feat common: add sum handler (#4)
    * feat common: add sum handler

--- app.py
+++ app.py
@@ -18,5 +18,14 @@ def sqrt():
     return jsonify(response)
 
 
+@app.route('/sum', methods=['GET'])
+def add_message_of_sum():
+    if 'value' not in request.args:
+        return 'No value in request', 400
+    total = sum([int(x) for x in request.args['value'].split('_')])
+    response = {'result': total, 'Message': 'Evthg is fuckin awesome'}
+    return jsonify(response)
+
+
 if __name__ == '__main__':
     app.run()
